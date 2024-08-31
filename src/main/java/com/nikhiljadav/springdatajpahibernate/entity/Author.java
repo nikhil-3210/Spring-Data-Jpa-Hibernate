59c724fe2640d1477ca532542a72719e58c06210
Nikhil Jadav <nikhil.jadhav3210@gmail.com>
2024-08-31 16:22:58 +0530

    many to many relationship added between author and course entity

--- src/main/java/com/nikhiljadav/springdatajpahibernate/entity/Author.java
+++ src/main/java/com/nikhiljadav/springdatajpahibernate/entity/Author.java
@@ -2,9 +2,12 @@ package com.nikhiljadav.springdatajpahibernate.entity;
 
 import com.nikhiljadav.springdatajpahibernate.entity.base.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -18,4 +21,7 @@ public class Author extends BaseEntity {
     private String firstname;
     private String lastname;
     private String email;
+
+    @ManyToMany(mappedBy = "authors")
+    private List<Course> courses;
 }
--- src/main/java/com/nikhiljadav/springdatajpahibernate/entity/Course.java
+++ src/main/java/com/nikhiljadav/springdatajpahibernate/entity/Course.java
@@ -1,13 +1,14 @@
 package com.nikhiljadav.springdatajpahibernate.entity;
 
 import com.nikhiljadav.springdatajpahibernate.entity.base.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -20,4 +21,16 @@ public class Course extends BaseEntity {
 
     private String title;
     private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "authors_courses",
+            joinColumns = {
+                    @JoinColumn(name = "course_id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "author_id")
+            }
+    )
+    private List<Author> authors;
 }
